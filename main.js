c2c5fc529d19fbb363f769294dfd0b93a9adcab3
KashviVijay <84574316+KashviVijay@users.noreply.github.com>
2021-08-01 15:49:45 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,18 @@
+function preload(){
+
+}
+
+function setup()
+{
+    canvas=createcanvas(300,300);
+    canvas.center();
+}
+
+function draw(){
+
+}
+
+function take_snapshot()
+{
+    save('myFilterImage.png');
+}
